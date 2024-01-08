ebebfb341cca0caba6f76c3aad7534f6b68fe617
Lorenzo Gualniera <lorenzo.gualniera@st.com>
2024-01-08 15:43:40 +0100

    mlc python script: add validate_results option to mlc_test
    Signed-off-by: Lorenzo Gualniera <lorenzo.gualniera@st.com>

--- tools/mlc_python_script/Script/mlc_test.py
+++ tools/mlc_python_script/Script/mlc_test.py
@@ -1,14 +1,31 @@
 import subprocess
-from mlc_script_log import *
+import logging
+
 import external_tools
 
-def test_arff_on_decisiontree( dectree_filename, arff_filename, testresults_filename, device_name ):  
+
+def test_arff_on_decisiontree(
+    dectree_filename: str,
+    arff_filename: str,
+    testresults_filename: str,
+    validate_results: bool = True
+):
     logging.info("\nCalling MLC app for testing ARFF on decision tree...")
-    args = [external_tools.mlc_app, "-" + device_name, "./CLI", "-MLC_test_arff", dectree_filename, arff_filename, testresults_filename]
-    mlc_app_ret_value = subprocess.call(args)
-    if (mlc_app_ret_value == 0):
-        logging.info("\nTest file generated successfully: " + testresults_filename)
-    elif (mlc_app_ret_value == 70):
-        logging.error("\nERROR: file not found")
+    proc = subprocess.run(
+        args=[
+            external_tools.mlc_app,
+            "dtt",
+            dectree_filename,
+            "--arff",
+            arff_filename,
+            "--arff-stats" if validate_results else "--pred-stats"
+        ],
+        stdout=subprocess.PIPE,
+        check=False
+    )
+    if proc.returncode == 0:
+        logging.info(f"\nTest file generated successfully: {testresults_filename}")
     else:
-        logging.error("\nERROR: ", mlc_app_ret_value)
+        logging.error(f"\nERROR: {proc.returncode}")
+    with open(testresults_filename, "wb") as f:
+        f.write(proc.stdout)
