4b765ba1fbb579a7aab02aafe276b31c2ac5a630
Lorenzo Gualniera <lorenzo.gualniera@st.com>
2024-05-13 16:17:52 +0200

    lis2dux12: add application examples
    Signed-off-by: Lorenzo Gualniera <lorenzo.gualniera@st.com>

---
+++ application_examples/lis2dux12/Vibration monitoring/README.md
@@ -0,0 +1,42 @@
+## 1 - Introduction
+
+In this example, a simple vibration monitoring algorithm is provided.
+
+The classes recognized by this example are: vibration1 (no vibration), vibration2 (low vibration), vibration3 (high vibration). 
+
+For information on how to integrate this algorithm in the target platform, please follow the instructions available in the README file of the [application_examples]( https://github.com/STMicroelectronics/STMems_Machine_Learning_Core/tree/master/application_examples ) folder. 
+
+For information on how to create similar algorithms, please follow the instructions provided in the [configuration_examples]( https://github.com/STMicroelectronics/STMems_Machine_Learning_Core/tree/master/configuration_examples ) folder. 
+
+
+## 2 - Sensor configuration and orientation
+
+The accelerometer is configured with ±4 *g* full scale and 25 Hz output data rate.
+
+Any sensor orientation is allowed for this algorithm.
+
+
+## 3 - Machine Learning Core configuration
+
+Just one feature has been used (peak-to-peak) and applied to the accelerometer norm squared input. 
+
+The MLC runs at 25 Hz, computing features on windows of 15 samples (around 0.6 seconds). 
+
+One decision tree with just 2 nodes has been configured to detect the different classes.
+A meta-classifier has not been used.  
+
+- MLC0_SRC (70h) register values
+  - 0 = vibration1 (no vibration)
+  - 1 = vibration2 (low vibration)
+  - 2 = vibration3 (high vibration)
+
+
+## 4 - Interrupts
+
+The configuration generates an interrupt (pulsed and active high) on the INT1 pin every time the register MLC0_SRC (70h) is updated with a new value. The duration of the interrupt pulse is 38.5 ms in this configuration.
+
+------
+
+**More Information: [http://www.st.com](http://st.com/MEMS)**
+
+**Copyright © 2020 STMicroelectronics**
---
+++ application_examples/lis2dux12/Vibration monitoring/lis2dux12_vibration_monitoring.h
@@ -0,0 +1,144 @@
+/**
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#ifndef LIS2DUX12_VIBRATION_MONITORING_H
+#define LIS2DUX12_VIBRATION_MONITORING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#ifndef MEMS_UCF_EXT_SHARED_TYPES
+#define MEMS_UCF_EXT_SHARED_TYPES
+
+/*
+ * UCF extended format supports the following commands:
+ * - MEMS_UCF_OP_READ: read the register at the location specified by the
+ *   "address" field ("data" field is ignored)
+ * - MEMS_UCF_OP_WRITE: write the value specified by the "data" field at the
+ *   location specified by the "address" field
+ * - MEMS_UCF_OP_DELAY: wait the number of milliseconds specified by the "data"
+ *   field ("address" field is ignored)
+ * - MEMS_UCF_OP_POLL_SET: poll the register at the location specified by the
+ *   "address" field until all the bits identified by the mask specified by the
+ *   "data" field are set to 1
+ * - MEMS_UCF_OP_POLL_RESET: poll the register at the location specified by the
+ *   "address" field until all the bits identified by the mask specified by the
+ *   "data" field are reset to 0
+ */
+
+#define MEMS_UCF_OP_READ       0
+#define MEMS_UCF_OP_WRITE      1
+#define MEMS_UCF_OP_DELAY      2
+#define MEMS_UCF_OP_POLL_SET   3
+#define MEMS_UCF_OP_POLL_RESET 4
+
+typedef struct {
+  uint8_t op;
+  uint8_t address;
+  uint8_t data;
+} ucf_line_ext_t;
+
+#endif /* MEMS_UCF_EXT_SHARED_TYPES */
+
+/** Configuration array generated from MLC Tool **/
+static const ucf_line_ext_t lis2dux12_vibration_monitoring[] = {
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x13, .data = 0x10 },
+  { .op = MEMS_UCF_OP_DELAY, .address = 0, .data = 5 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x14, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x3F, .data = 0x80 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x04, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x05, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x17, .data = 0x40 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x02, .data = 0x01 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x08, .data = 0xB8 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0xE6 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0xF0 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x0A },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x01 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x0F },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x02, .data = 0x01 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x08, .data = 0xC8 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0xDC },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0xF2 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0xFE },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x02, .data = 0x01 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x08, .data = 0xDC },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x3F },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x03 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x94 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0xFC },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x7C },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x1F },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x02, .data = 0x01 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x08, .data = 0xF2 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x3F, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x3F, .data = 0x80 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x17, .data = 0x40 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x02, .data = 0x01 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x08, .data = 0xFE },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0xAE },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x27 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x02, .data = 0x11 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x08, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x10 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0xC0 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x3E },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0x21 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x09, .data = 0xE0 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x3F, .data = 0x80 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x17, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x04, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x05, .data = 0x10 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x02, .data = 0x01 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x3F, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x1F, .data = 0x01 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x3F, .data = 0x80 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x0D, .data = 0x01 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x3A, .data = 0x11 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x3F, .data = 0x00 },
+  { .op = MEMS_UCF_OP_WRITE, .address = 0x14, .data = 0x61 }
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIS2DUX12_VIBRATION_MONITORING_H */
+
